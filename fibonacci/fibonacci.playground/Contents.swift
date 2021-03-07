b68347efc7c2c33d1606307fd997b2c514c8558d
Vitor Oliveira <vbrazo@gmail.com>
2021-03-07 14:00:52 -0800

    Update Contents.swift

--- fibonacci/fibonacci.playground/Contents.swift
+++ fibonacci/fibonacci.playground/Contents.swift
@@ -1,4 +1,12 @@
-// A simple code about Fibonacci recursive form 
+// The Fibonacci numbers, commonly denoted F(n) form a sequence,
+// called the Fibonacci sequence, such that # each number is the sum
+// of the two preceding ones, starting from 0 and 1. That is,
+//
+// F(0) = 0, F(1) = 1
+// F(n) = F(n - 1) + F(n - 2), for n > 1
+//
+// Given n, calculate F(n).
+//
 // @leticiafaleia
 
 func fibonacci(_ number: Int) -> Int {
